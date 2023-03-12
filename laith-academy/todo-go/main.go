305eebe9511cc813f508462387661f2e4142384a
M-Facey <faceymaxal17@gmail.com>
2023-03-12 13:01:40 -0400

    feat: completed simple api using gin

--- laith-academy/todo-go/main.go
+++ laith-academy/todo-go/main.go
@@ -1,1 +1,79 @@
 package main
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type todo struct {
+	ID        string `json:"id"`
+	Item      string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+var todos = []todo{
+	{ID: "1", Item: "Clean Room", Completed: false},
+	{ID: "2", Item: "Read Books", Completed: false},
+	{ID: "3", Item: "Write Code", Completed: false},
+}
+
+func getTodos(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, todos)
+}
+
+func addTodo(context *gin.Context) {
+	var newTodo todo
+	if err := context.BindJSON(&newTodo); err != nil {
+		return
+	}
+
+	todos = append(todos, newTodo)
+
+	context.IndentedJSON(http.StatusCreated, newTodo)
+}
+
+func getTodoById(id string) (*todo, error) {
+	for i, t := range todos {
+		if t.ID == id {
+			return &todos[i], nil
+		}
+	}
+	return nil, errors.New("TODO NOT FOUND")
+}
+
+func getTodo(context *gin.Context) {
+	id := context.Param("id")
+	todo, err := getTodoById(id)
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo was not found!"})
+		return
+	}
+	context.IndentedJSON(http.StatusOK, todo)
+}
+
+func toggleTodoStatus(context *gin.Context) {
+	id := context.Param("id")
+	todo, err := getTodoById(id)
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo was not found!"})
+		return
+	}
+
+	todo.Completed = !todo.Completed
+	context.IndentedJSON(http.StatusOK, todo)
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/api/v1/todos", getTodos)
+	router.GET("/api/v1/todos/:id", getTodo)
+	router.PATCH("/api/v1/todos/:id", toggleTodoStatus)
+	router.POST("/api/v1/todos", addTodo)
+
+	router.Run("localhost:9090")
+}
